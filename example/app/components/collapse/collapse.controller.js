53bc5d97166179307deb5f22186888ae9117d6ee
dongyu12 <dongyu12@wanda.cn>
2017-02-18 18:15:46 +0800

    20170218

--- example/app/components/collapse/collapse.controller.js
+++ example/app/components/collapse/collapse.controller.js
@@ -11,18 +11,14 @@ export default class CollapseController extends Base{
   constructor($state,$translate) {
     'ngInject';
     super($state,$translate);
-    this.datas = [
+    this.data = [
       {title: '标题1', content: '内容1'},
       {title: '标题2', content: '内容2'},
       {title: '标题3', content: '内容3'},
       {title: '标题4', content: '内容4'},
-      {title: '标题5', content: '内容5'},
+      {title: '标题5', content: '内容5'}
 
     ]
-    this.data = {
-      title: '标题',
-      content: '内容'
-    }
   }
   selectItem(index){
 
